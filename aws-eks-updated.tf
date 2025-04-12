4bb4065e5254c71a441c67279d31ad8bc559c330
Dr CHANDRA PRAKASH KADARLA <ckadarla@users.noreply.github.com>
2025-04-12 18:33:12 +0530

    Update aws-eks-updated.tf

--- aws-eks-updated.tf
+++ aws-eks-updated.tf
@@ -1,24 +1,143 @@
-# main.tf
-provider "google" {
-  project = "my-gcp-project"
-  region  = "us-central1"
+# provider.tf
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+# vpc.tf
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "eks-subnet-a"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  tags = {
+    Name = "eks-subnet-b"
+  }
+}
+
+# eks_cluster.tf
+resource "aws_eks_cluster" "main" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_a.id,
+      aws_subnet.subnet_b.id
+    ]
   }
 
-  network_interface {
-    network       = "default"
-    access_config {}
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_role_policy
+  ]
+}
+
+# eks_nodegroup.tf
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "example-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  instance_types  = ["t3.medium"]
+  desired_size    = 2
+  max_size        = 3
+  min_size        = 1
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  tags = ["terraform", "test"]
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+}
+
+# iam.tf (IAM Roles)
+resource "aws_iam_role" "eks_cluster_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect   = "Allow"
+        Sid      = ""
+      }
+    ]
+  })
+
+  tags = {
+    Name = "eks-cluster-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect   = "Allow"
+        Sid      = ""
+      }
+    ]
+  })
+
+  tags = {
+    Name = "eks-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role_policy_vpc" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# output.tf
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS Cluster Endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "EKS Cluster Security Group ID"
+  value       = aws_eks_cluster.main.cluster_security_group_id
 }
